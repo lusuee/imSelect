64889afac9e3dd7d854206c6d04b936cad3c9711
lusuee <1349543950@qq.com>
2022-06-30 11:36:17 +0800

    修改 im-select 路径

--- src/im-select.js
+++ src/im-select.js
@@ -1,63 +1,71 @@
 const child_process = require("child_process");
 const util = require("util");
-const path = "/opt/homebrew/bin/im-select";
+// const path = "/opt/homebrew/bin/im-select";
 let isRun = false;
+let path = "/usr/local/bin/im-select";
 
 function plugin(CodeMirror) {
-	if (isRun) return;
+  if (isRun) return;
 
-	let im;
-	let imDefault = "com.apple.keylayout.ABC";
-	let i = util.promisify(imChange);
-	let old = CodeMirror.Vim.findKey;
-	CodeMirror.Vim.findKey = function (cm, key, origin) {
-		let p = old(cm, key, origin);
-		i(key);
-		return p;
-	};
+  // 不能通过该命令获取到 im-select 路径，执行该命令报错
+  //   child_process.execSync("which im-select", (err, stdout, stderr) => {
+  //     console.log(err);
+  //     console.log(stdout);
+  //     console.log(stderr);
+  //   });
 
-	function imChange(key) {
-		if (key === "<Esc>") {
-			child_process.exec(
-				path,
-				{ shell: "/bin/zsh", timeout: 500, windowsHide: true },
-				(err, stdout, stderr) => {
-					im = stdout;
-				}
-			);
-			child_process.exec(path + " " + imDefault, {
-				shell: "/bin/zsh",
-				timeout: 500,
-				windowsHide: true,
-			});
-		} else if (
-			key === "i" ||
-			key === "I" ||
-			key === "a" ||
-			key === "A" ||
-			key === "o" ||
-			key === "O" ||
-			key === "s" ||
-			key === "S"
-		) {
-			if (!im) {
-				im = imDefault;
-			}
-			child_process.exec(path + " " + im, {
-				shell: "/bin/zsh",
-				timeout: 500,
-				windowsHide: true,
-			});
-		}
-	}
+  let im;
+  let imDefault = "com.apple.keylayout.ABC";
+  let i = util.promisify(imChange);
+  let old = CodeMirror.Vim.findKey;
+  CodeMirror.Vim.findKey = function (cm, key, origin) {
+    let p = old(cm, key, origin);
+    i(key);
+    return p;
+  };
 
-	isRun = true;
+  function imChange(key) {
+    if (key === "<Esc>") {
+      child_process.exec(
+        path,
+        { shell: "/bin/zsh", timeout: 500, windowsHide: true },
+        (err, stdout, stderr) => {
+          im = stdout;
+        }
+      );
+      child_process.exec(path + " " + imDefault, {
+        shell: "/bin/zsh",
+        timeout: 500,
+        windowsHide: true,
+      });
+    } else if (
+      key === "i" ||
+      key === "I" ||
+      key === "a" ||
+      key === "A" ||
+      key === "o" ||
+      key === "O" ||
+      key === "s" ||
+      key === "S"
+    ) {
+      if (!im) {
+        im = imDefault;
+      }
+      child_process.exec(path + " " + im, {
+        shell: "/bin/zsh",
+        timeout: 500,
+        windowsHide: true,
+      });
+    }
+  }
+
+  isRun = true;
 }
 
 module.exports = {
-	default: function (_context) {
-		return {
-			plugin: plugin,
-		};
-	},
+  default: function (_context) {
+    return {
+      plugin: plugin,
+    };
+  },
 };
